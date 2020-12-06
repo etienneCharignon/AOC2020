52e20fc210cf040d07bb4cda4adc1342eecd5d7c
Étienne Charignon <etienne.charignon@gmail.com>
2020-12-06 11:22:13 +0100

    Day 6 part 2 😅

--- lib/day6.rb
+++ lib/day6.rb
@@ -1,7 +1,17 @@
 require './data_day6'
 
+# INPUT = %{a
+# b
+# c}
+
 GROUPS_INPUTS = INPUT.split("\n\n")
-groups_counts = GROUPS_INPUTS.map do |group_inputs|
-  group_inputs.gsub(/\n/, '').split('').uniq.count
+groups_counts = GROUPS_INPUTS.map do |group_responses_string|
+  group_responses = group_responses_string.split("\n")
+  group_yes = group_responses[0].split('')
+  group_responses.each do |personne_responses|
+    group_yes.clone.each { |response| group_yes.delete(response) unless personne_responses.include?(response) }
+  end
+  group_yes.size
 end
+p groups_counts
 p groups_counts.inject(0, :+)
