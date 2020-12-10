7a5624e303ddc8d372f929f33aa41dbd0b75cd68
Étienne Charignon <etienne.charignon@gmail.com>
2020-12-10 15:52:09 +0100

    day 10 part 2

--- lib/joltage_adapter.rb
+++ lib/joltage_adapter.rb
@@ -3,11 +3,35 @@ class JoltageAdapter
     sorted = adapters.sort
     sorted << sorted.max + 3
     sorted.unshift(0)
-    (0..sorted.length-1).each_with_object({:one => 0, :three => 0}) do |index, counters|
+    (1..sorted.length-1).each_with_object({:one => 0, :three => 0}) do |index, counters|
       difference = sorted[index] - sorted[index-1]
-      p difference
       counters[:one] += 1 if difference == 1
       counters[:three] += 1 if difference == 3
     end.values.inject(:*)
+  end
+
+  def self.count1(adapter, arcs)
+    return 1 if adapter == 0
+    arcs[adapter].map { |connected| count(connected, arcs) }.inject(:+)
+  end
+
+  def self.count2(adapter, arcs)
+    return 1 if adapter == 0
+    return arcs[adapter] unless arcs[adapter].kind_of?(Array)
+    arcs[adapter].map { |connected| count2(connected, arcs) }.inject(:+)
+  end
+
+  def self.find_arrangements(adapters)
+    sorted = adapters.sort.unshift(0)
+    sorted << sorted.max + 3
+    arcs = (0..sorted.length-1).each_with_object({}) do |index, arcs|
+      arcs[sorted[index]] = [sorted[index-1]]
+      arcs[sorted[index]] << sorted[index-2] if index > 1 && sorted[index] - sorted[index-2] < 4
+      arcs[sorted[index]] << sorted[index-3] if index > 2 && sorted[index] - sorted[index-3] < 4
+    end
+    arcs.keys.sort.each do |adapter|
+      arcs[adapter] = count2(adapter, arcs)
+    end
+    arcs[sorted[-1]]
   end
 end
--- spec/10_jotage_spec.rb
+++ spec/10_jotage_spec.rb
@@ -1,17 +1,8 @@
 require 'joltage_adapter'
 require 'data_day10'
 
-EXAMPLE_INPUT_10 = [16,
-10,
-15,
-5,
-1,
-11,
-7,
-19,
-6,
-12,
-4]
+EXAMPLE_INPUT_10 = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]
+EXAMPLE2_INPUT_10 = [28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3]
 
 RSpec.describe JoltageAdapter do
   describe "Part 1" do
@@ -19,7 +10,18 @@ RSpec.describe JoltageAdapter do
       expect(JoltageAdapter.find(EXAMPLE_INPUT_10)).to eql(5*7)
     end
     it 'find the differences in the input' do
-      expect(JoltageAdapter.find(INPUT10)).to eql(5*7)
+      expect(JoltageAdapter.find(INPUT10)).to eql(1625)
+    end
+  end
+
+
+  describe "Part 2" do
+    describe 'find arrangement' do
+      it { expect(JoltageAdapter.count2(1, { 0=>1, 1=>[0] })).to eql(1) }
+      it { expect(JoltageAdapter.count2(2, { 0=>1, 1=>1, 2=>[1,0] })).to eql(2) }
+      it { expect(JoltageAdapter.find_arrangements(EXAMPLE_INPUT_10)).to eql(8) }
+      it { expect(JoltageAdapter.find_arrangements(EXAMPLE2_INPUT_10)).to eql(19208) }
+      it { expect(JoltageAdapter.find_arrangements(INPUT10)).to eql(3100448333024) }
     end
   end
 end
