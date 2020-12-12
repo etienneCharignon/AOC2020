13eb5f40b39fc01002533027ceef3692b2661d6a
Étienne Charignon <etienne.charignon@gmail.com>
2020-12-12 10:06:10 +0100

    Refactor part 1

--- lib/rain_risk.rb
+++ lib/rain_risk.rb
@@ -1,24 +1,31 @@
 class RainRisk
 
+  attr_reader :moves
+
   def initialize(direction)
     @direction = direction
+    @moves = {
+      'E' => 0,
+      'S' => 0,
+      'W' => 0,
+      'N' => 0,
+    }
   end
 
-  def process_from(position, instruction)
+  def process_from(instruction)
     steps = instruction[1..-1].to_i
-    position.each_key do |direction|
-      position[direction] += steps if instruction.start_with?(direction)
+    @moves.each_key do |direction|
+      @moves[direction] += steps if instruction.start_with?(direction)
     end
     if instruction.start_with?('L')
       rotation = steps/90
-      @direction = position.keys[position.keys.index(@direction) - rotation]
+      @direction = @moves.keys[@moves.keys.index(@direction) - rotation]
     end
     if instruction.start_with?('R')
       rotation = steps/90
-      @direction = position.keys[(position.keys.index(@direction) + rotation)%4]
+      @direction = @moves.keys[(@moves.keys.index(@direction) + rotation)%4]
     end
-    position[@direction] += steps if instruction.start_with?('F')
-    position
+    @moves[@direction] += steps if instruction.start_with?('F')
   end
 
   def self.process(instructions)
@@ -28,16 +35,10 @@ class RainRisk
       'W' => 0,
       'N' => 1,
     }
-    moves = {
-      'E' => 0,
-      'S' => 0,
-      'W' => 0,
-      'N' => 0,
-    }
     boat = RainRisk.new('E')
     instructions.split("\n").each do |instruction|
-      moves = boat.process_from(moves, instruction)
+      boat.process_from(instruction)
     end
-    moves
+    boat.moves
   end
 end
