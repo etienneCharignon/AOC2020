7aaec975f8a0e7f52db1dcc6e723541bf2c2ea39
Étienne Charignon <etienne.charignon@gmail.com>
2020-12-12 10:52:58 +0100

    Day 12 part 2

--- lib/rain_risk.rb
+++ lib/rain_risk.rb
@@ -1,6 +1,6 @@
 class RainRisk
 
-  attr_reader :moves
+  attr_reader :moves, :waypoint
 
   def initialize(direction)
     @direction = direction
@@ -9,6 +9,12 @@ class RainRisk
       'S' => 0,
       'W' => 0,
       'N' => 0,
+    }
+    @waypoint = {
+      'E' => 10,
+      'S' => 0,
+      'W' => 0,
+      'N' => 1,
     }
   end
 
@@ -28,16 +34,46 @@ class RainRisk
     @moves[@direction] += steps if instruction.start_with?('F')
   end
 
+  def rotate(position, rotation)
+    new_position = {}
+    position.each_key do |direction|
+      nouvelle_direction = position.keys[(position.keys.index(direction) + rotation)%4]
+      new_position[nouvelle_direction] = position[direction]
+    end
+    new_position
+  end
+
+  def process2_from(instruction)
+    steps = instruction[1..-1].to_i
+    @waypoint.each_key do |direction|
+      @waypoint[direction] += steps if instruction.start_with?(direction)
+    end
+    if instruction.start_with?('R')
+      @waypoint = rotate(@waypoint, steps/90)
+    end
+    if instruction.start_with?('L')
+      @waypoint = rotate(@waypoint, -steps/90)
+    end
+    if instruction.start_with?('F')
+      @waypoint.each_key do |direction|
+        @moves[direction] += steps * @waypoint[direction]
+      end
+    end
+    self
+  end
+
   def self.process(instructions)
-    waypoint = {
-      'E' => 10,
-      'S' => 0,
-      'W' => 0,
-      'N' => 1,
-    }
     boat = RainRisk.new('E')
     instructions.split("\n").each do |instruction|
       boat.process_from(instruction)
+    end
+    boat.moves
+  end
+
+  def self.process2(instructions)
+    boat = RainRisk.new('E')
+    instructions.split("\n").each do |instruction|
+      boat.process2_from(instruction)
     end
     boat.moves
   end
--- spec/12_rain_risk_spec.rb
+++ spec/12_rain_risk_spec.rb
@@ -13,4 +13,11 @@ RSpec.describe RainRisk do
     it { expect(RainRisk.process("R360\nF1")).to eql({'N' => 0, 'S' => 0, 'E' => 1, 'W' => 0}) }
     it { expect(RainRisk.process(INPUT12)).to eql({'N' => 3163, 'S' => 3403, 'E' => 2565, 'W' => 2833}) }
   end
+  describe 'process instructions part 2' do
+    it { expect(RainRisk.process2("F10")).to eql({'N' => 10, 'S' => 0, 'E' => 100, 'W' => 0}) }
+    it { expect(RainRisk.process2("N1\nF1")).to eql({'N' => 2, 'S' => 0, 'E' => 10, 'W' => 0}) }
+    it { expect(RainRisk.new('E').process2_from("R90").waypoint).to eql({'N' => 0, 'S' => 10, 'E' => 1, 'W' => 0}) }
+    it { expect(RainRisk.new('E').process2_from("L90").waypoint).to eql({'N' => 10, 'S' => 0, 'E' => 0, 'W' => 1}) }
+    it { expect(RainRisk.process2(INPUT12)).to eql({'N' => 1649161, 'S' => 1666334, 'E' => 1651476, 'W' => 1665064}) }
+  end
 end
