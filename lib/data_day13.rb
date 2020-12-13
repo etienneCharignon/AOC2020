45be225958f90fe06d991a73bc7bb7b490195a43
Étienne Charignon <etienne.charignon@gmail.com>
2020-12-13 09:33:39 +0100

    Day 13 part 1

---
+++ lib/data_day13.rb
@@ -0,0 +1,6 @@
+INPUT13_TS = 1009310
+INPUT13_BUS = [19,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,37,nil,nil,nil,nil,nil,599,nil,29,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,17,nil,nil,nil,nil,nil,23,nil,nil,nil,nil,nil,nil,nil,761,nil,nil,nil,nil,nil,nil,nil,nil,nil,41,nil,nil,13]
+
+EXAMPLE_TS = 939
+EXAMPLE_BUS = [7,13,nil,nil,59,nil,31,19]
+
---
+++ lib/day13.rb
@@ -0,0 +1,26 @@
+require './data_day13'
+
+def compute(ts, buses)
+  departures = buses.map do |bus|
+    if ! bus
+      nil
+    else
+      ts / bus * bus + bus
+    end
+  end
+  p buses
+  p departures
+
+  departure = departures.min
+  bus_to_take = buses[departures.index(departure)]
+  minutes_to_wait =  departure - ts
+
+  p departure
+  p bus_to_take
+  p minutes_to_wait
+  p bus_to_take * (departure - ts)
+end
+
+compute(INPUT13_TS, INPUT13_BUS.compact)
+puts
+compute(EXAMPLE_TS, EXAMPLE_BUS.compact)
