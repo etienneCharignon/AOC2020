d13845581f023286d5c3d51004dcd6ea97e3321e
Étienne Charignon <etienne.charignon@gmail.com>
2020-12-15 18:06:39 +0100

    Day 15 part 2

--- spec/15_rambunctious_recitation_spec.rb
+++ spec/15_rambunctious_recitation_spec.rb
@@ -3,32 +3,60 @@ def recitation(start, n)
   spoken = {}
   start.each_with_index { |n, i| spoken[n] = [i+1] }
   spoke = start[-1]
-    p spoken
   (start.size+1..n).each do |i|
     if spoken.keys.include? spoke
-      if spoken[spoke].size == 1
+      data_spoken = spoken[spoke]
+      if data_spoken.size == 1
         spoke = 0
       else
-        spoke = spoken[spoke][-1] - spoken[spoke][-2]
+        spoke = data_spoken[-1] - data_spoken[-2]
       end
     else
       spoke = 0
     end
     spoken[spoke] ||= []
     spoken[spoke] << i
+    spoken[spoke].shift if spoken[spoke].size > 2
+  end
+  spoke
+end
+
+def recitation2(start, n)
+  spoken = []
+  start.each_with_index { |n, i| spoken[n] = [i+1] }
+  spoke = start[-1]
+  p spoken
+  (start.size+1..n).each do |i|
+    if spoken[spoke] != nil
+      data_spoken = spoken[spoke]
+      if data_spoken.size == 1
+        spoke = 0
+      else
+        spoke = data_spoken[-1] - data_spoken[-2]
+      end
+    else
+      spoke = 0
+    end
+    spoken[spoke] ||= []
+    spoken[spoke] << i
+    spoken[spoke].shift if spoken[spoke].size > 2
   end
   spoke
 end
 
 RSpec.describe "Rambunctious Recitation" do
   describe "Part 1" do
-    xit { expect(recitation([0,3,6], 1)).to eql(0)}
-    it { expect(recitation([0,3,6], 3)).to eql(6)}
-    it { expect(recitation([0,3,6], 4)).to eql(0)}
-    it { expect(recitation([0,3,6], 5)).to eql(3)}
-    it { expect(recitation([0,3,6], 6)).to eql(3)}
-    it { expect(recitation([0,3,6], 7)).to eql(1)}
-    it { expect(recitation([0,3,6], 2020)).to eql(436)}
-    it { expect(recitation([1,20,8,12,0,14], 2020)).to eql(492)}
+    it { expect(recitation2([0,3,6], 3)).to eql(6)}
+    it { expect(recitation2([0,3,6], 4)).to eql(0)}
+    it { expect(recitation2([0,3,6], 5)).to eql(3)}
+    it { expect(recitation2([0,3,6], 6)).to eql(3)}
+    it { expect(recitation2([0,3,6], 7)).to eql(1)}
+    it { expect(recitation2([0,3,6], 2020)).to eql(436)}
+    it { expect(recitation2([1,20,8,12,0,14], 2020)).to eql(492)}
+  end
+
+  describe "Part 2" do
+    it { expect(recitation2([0,3,6], 30000000)).to eql(175594)}
+    it { expect(recitation2([1,20,8,12,0,14], 30000000)).to eql(492)}
   end
 end
