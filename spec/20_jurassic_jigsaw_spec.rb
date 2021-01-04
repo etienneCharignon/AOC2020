ff2014afd16c35ba919e46dd4b883610b3fa7604
Étienne Charignon <etienne.charignon@gmail.com>
2021-01-04 08:48:28 +0100

    Assemble it ! finaly !

--- spec/20_jurassic_jigsaw_spec.rb
+++ spec/20_jurassic_jigsaw_spec.rb
@@ -48,58 +48,66 @@ Tile 1951:
 # ##...##.#.
 # ..###.#.#.
 
+class Tile
+  attr_reader :id, :bords, :tile, :bords_and_reverse
+
+  def initialize(rows)
+    @id = rows[0][/\d+/].to_i
+    @tile = rows[1..-1]
+    @bords = extract_borders(@tile)
+    @bords_and_reverse = @bords + @bords.map { |b| b.reverse }
+  end
+
+  def extract_borders(rows)
+    top = rows[0]
+    right = rows[0..-1].map {|r| r[-1]}.join('')
+    bottom = rows[-1]
+    left = rows[0..-1].map {|r| r[0]}.join('')
+    [top, right, bottom, left]
+  end
+
+  def flip(axe)
+    if axe == :h
+      @bords = [@bords[2], @bords[1].reverse, @bords[0], @bords[3].reverse]
+      @tile = @tile.reverse
+    else
+      @bords = [@bords[0].reverse, @bords[3], @bords[2].reverse, @bords[1]]
+      @tile = @tile.map { |r| r.reverse }
+    end
+    self
+  end
+
+  def rotate
+    @tile = @tile.map.with_index(0) { |_, i| @tile.reverse.map { |r| r[i] }.join('') }
+    @bords = extract_borders(@tile)
+    self
+  end
+end
+
 def read_20(input)
   tiles = {}
   input.split("Tile").drop(1).each do |tile_input|
     rows = tile_input.split("\n")
-    top = rows[1]
-    right = rows[1..-1].map {|r| r[-1]}.join('')
-    bottom = rows[-1].reverse
-    left = rows[1..-1].map {|r| r[0]}.join('')
-    tiles[rows[0][/\d+/].to_i] = { bords: [top, right, bottom, left], tile: rows[1..-1] }
+    tile = Tile.new(rows)
+    tiles[tile.id] = tile
   end
   tiles
 end
 
-def flip(tiles, id, axe)
-  bords = tiles[id][:bords]
-  if axe == :h
-    tiles[id][:bords] = [bords[2], bords[1].reverse, bords[0], bords[3].reverse]
-    tiles[id][:tile] = tiles[id][:tile].reverse
-  else
-    tiles[id][:bords] = [bords[0].reverse, bords[3], bords[2].reverse, bords[1]]
-    tiles[id][:tile] = tiles[id][:tile].map { |r| r.reverse }
-  end
-end
-
-def rotate(tiles, id)
-  tile = tiles[id][:bords]
-  tiles[id][:bords] = [tile[3], tile[0], tile[1], tile[2]]
-end
-
-def add_reverse(tiles)
-  tiles_with_reverse = {}
-  tiles.each do |id, tile|
-    tiles_with_reverse[id] = tile[:bords] + tile[:bords].map { |b| b.reverse }
-  end
-  tiles_with_reverse
-end
-
-def has_neighbor(id, border, tiles_with_reverse)
-  tiles_with_reverse.each do |tid, borders|
-    return tid if tid != id && borders.include?(border)
+def has_neighbor(id, border, tiles)
+  tiles.each do |tid, tile|
+    return tid if tid != id && tile.bords_and_reverse.include?(border)
   end
   return nil
 end
 
 def find_corners(tiles)
-  tiles_with_reverse = add_reverse(tiles)
   corner = []
   tiles.each do |id, tile|
     #p "NEW TILE #{id}"
     no_neighbour_border_count = 0
-    for b in tile[:bords]
-      has = has_neighbor(id, b, tiles_with_reverse) != nil
+    for b in tile.bords
+      has = has_neighbor(id, b, tiles) != nil
       no_neighbour_border_count += 1 unless has
       if no_neighbour_border_count == 2
         corner << id
@@ -110,32 +118,52 @@ def find_corners(tiles)
   corner
 end
 
-def find_neighbour(id, border, tiles_with_reverse)
-  tiles_with_reverse.each do |tid, borders|
-    return [tid, borders.index(border) ] if tid != id && borders.include?(border)
+def find_neighbour(id, border, tiles)
+  tiles.each do |tid, tile|
+    return tid if tid != id && tile.bords_and_reverse.include?(border)
   end
   return nil
 end
 
 def assemble(tiles, first_corner)
-  flip(tiles, first_corner, :h)
-  tiles_with_reverse = add_reverse(tiles)
+  tiles[first_corner].flip(:h)
   puzzle = []
   current_first_col_tile = first_corner
-  1.times do
-    puzzle += tiles[current_first_col_tile][:tile]
+  12.times do
+    puzzle << ""
+    puzzle += tiles[current_first_col_tile].tile
     puzzle_row = puzzle.size - 10
     current_row_tile = current_first_col_tile
-    12.times do
-      neighbour = find_neighbour(current_row_tile, tiles[current_row_tile][:bords][1], tiles_with_reverse)
+    11.times do
+      neighbour = find_neighbour(current_row_tile, tiles[current_row_tile].bords[1], tiles)
       break if neighbour.nil?
-      p neighbour
-      flip(tiles, neighbour[0], :h) if neighbour[1] == 7
-      tile.each_with_index { |r, i| puzzle[puzzle_row + i] += " #{tiles[neighbour[0]][:tile][i]}" }
-      current_row_tile = neighbour[0]
-    end
-    neighbour = find_neighbour(current_row_tile, tiles[current_row_tile][:bords][2], tiles_with_reverse)
 
+      while !(
+          tiles[current_row_tile].bords[1] == tiles[neighbour].bords[3] or
+          tiles[current_row_tile].bords[1] == tiles[neighbour].bords[3].reverse
+      ) do
+        tiles[neighbour].rotate
+      end
+      tiles[neighbour].flip(:h) if tiles[current_row_tile].bords[1] != tiles[neighbour].bords[3]
+
+      current_row_tile = neighbour
+
+      tiles[current_row_tile].tile.each_with_index { |r, i|
+        puzzle[puzzle_row + i] += " #{tiles[current_row_tile].tile[i]}"
+      }
+    end
+    neighbour = find_neighbour(current_first_col_tile, tiles[current_first_col_tile].bords[2], tiles)
+    break if neighbour.nil?
+
+    while !(
+        tiles[current_first_col_tile].bords[2] == tiles[neighbour].bords[0] or
+        tiles[current_first_col_tile].bords[2] == tiles[neighbour].bords[0].reverse
+    ) do
+      tiles[neighbour].rotate
+    end
+    tiles[neighbour].flip(:v) if tiles[current_first_col_tile].bords[2] != tiles[neighbour].bords[0]
+
+    current_first_col_tile = neighbour
   end
   puzzle.join("\n")
 end
@@ -143,36 +171,77 @@ end
 RSpec.describe "Jurassic Jigsaw", focus: true do
   describe "Part 1 : reassembled back into a single image" do
     describe "read the input" do
-      it { expect(read_20(ONE_TILE)).to eql({
-        2311 => {
-          :bords => ["..##.#..#.", "...#.##..#", "###..###..", ".#####..#."],
-          :tile => ["..##.#..#.", "##..#.....", "#...##..#.", "####.#...#", "##.##.###.", "##...#.###", ".#.#.#..##", "..#....#..", "###...#.#.", "..###..###"]
-        }
-      }) }
+      it { expect(read_20(ONE_TILE)[2311].bords)
+        .to eql(["..##.#..#.", "...#.##..#", "..###..###", ".#####..#."]) }
+      it { expect(read_20(ONE_TILE)[2311].tile)
+        .to eql(["..##.#..#.", "##..#.....", "#...##..#.", "####.#...#", "##.##.###.", "##...#.###", ".#.#.#..##", "..#....#..", "###...#.#.", "..###..###"]) }
       it { expect(read_20(TWO_TILES).keys).to eql([2311, 1951]) }
-      it { expect(read_20(TWO_TILES)[2311][:bords]).to eql(["..##.#..#.", "...#.##..#", "###..###..", ".#####..#."]) }
-      it { expect(read_20(TWO_TILES)[1951][:bords]).to eql(["#.##...##.", ".#####..#.", "..#.##...#", "##.#..#..#"]) }
+      it { expect(read_20(TWO_TILES)[2311].bords).to eql(["..##.#..#.", "...#.##..#", "..###..###", ".#####..#."]) }
+      it { expect(read_20(TWO_TILES)[1951].bords).to eql(["#.##...##.", ".#####..#.", "#...##.#..", "##.#..#..#"]) }
       it { expect(read_20(INPUTS20).count).to eql(144) }
     end
 
     describe "flip a tile" do
-      it { expect(flip({2311 => { bords: ["..##.#..#.", "...#.##..#", "###..###..", ".#####..#."]}}, 2311, :h))
-        .to eql(["###..###..", "...#.##..#".reverse, "..##.#..#.", ".#####..#.".reverse]) }
-      it { expect(flip({2311 => { bords: ["..##.#..#.", "...#.##..#", "###..###..", ".#####..#."]}}, 2311, :v))
-        .to eql(["..##.#..#.".reverse, ".#####..#.", "###..###..".reverse, "...#.##..#"]) }
+      it {
+        tile = read_20(ONE_TILE)[2311]
+        tile.flip(:h)
+        expect(tile.bords).to eql(["..###..###", "...#.##..#".reverse, "..##.#..#.", ".#####..#.".reverse])
+        expect(tile.tile).to eql([
+          "..###..###",
+          "###...#.#.",
+          "..#....#..",
+          ".#.#.#..##",
+          "##...#.###",
+          "##.##.###.",
+          "####.#...#",
+          "#...##..#.",
+          "##..#.....",
+          "..##.#..#."
+        ])
+      }
+      it {
+        tile = read_20(ONE_TILE)[2311]
+        tile.flip(:v)
+        expect(tile.bords).to eql(["..##.#..#.".reverse, ".#####..#.", "..###..###".reverse, "...#.##..#"])
+        expect(tile.tile).to eql([
+          "..##.#..#.".reverse,
+          "##..#.....".reverse,
+          "#...##..#.".reverse,
+          "####.#...#".reverse,
+          "##.##.###.".reverse,
+          "##...#.###".reverse,
+          ".#.#.#..##".reverse,
+          "..#....#..".reverse,
+          "###...#.#.".reverse,
+          "..###..###".reverse
+        ])
+      }
     end
 
     describe "rotate a tile" do
-      it { expect(rotate({2311 => { bords: ["..##.#..#.", "...#.##..#", "###..###..", ".#####..#."] }}, 2311))
-        .to eql([".#####..#.", "..##.#..#.", "...#.##..#", "###..###.."]) }
+      it {
+        tile = read_20(ONE_TILE)[2311]
+        tile.rotate
+        expect(tile.bords).to eql([".#..#####.", "..##.#..#.", "#..##.#...", "..###..###"])
+        expect(tile.tile).to eql([
+          ".#..#####.",
+          ".#.####.#.",
+          "###...#..#",
+          "#..#.##..#",
+          "#....#.##.",
+          "...##.##.#",
+          ".#...#....",
+          "#.#.##....",
+          "##.###.#.#",
+          "#..##.#..."
+        ])
+      }
     end
 
     describe "look for corners" do
-      it { expect(has_neighbor(1214, "..##.#..#.", { 1212 => ["..##.#..#.", "...#.##..#", "###..###..", ".#####..#."] })).to be(1212) }
-      it { expect(has_neighbor(1212, "..##.#..#.", { 1212 => ["..##.#..#.", "...#.##..#", "###..###..", ".#####..#."] })).to be(nil) }
-      it { expect(has_neighbor(1214, "..##.#....", { 1212 => ["..##.#..#.", "...#.##..#", "###..###..", ".#####..#."] })).to be(nil) }
-      it { expect(add_reverse(read_20(EXAMPLE_INPUT20))[1951]).to eql(["#.##...##.", ".#####..#.", "..#.##...#", "##.#..#..#",
-                                                                       ".##...##.#", ".#..#####.", "#...##.#..", "#..#..#.##"]) }
+      it { expect(has_neighbor(1214, "..##.#..#.", read_20(ONE_TILE))).to be(2311) }
+      it { expect(has_neighbor(1214, "..........", read_20(ONE_TILE))).to be(nil) }
+      it { expect(has_neighbor(2311, "..##.#..#.", read_20(ONE_TILE))).to be(nil) }
       it { expect(find_corners(read_20(EXAMPLE_INPUT20))).to eql([1951, 1171, 2971, 3079]) }
       it { expect(find_corners(read_20(INPUTS20))).to eql([3643, 2647, 1987, 3089]) }
     end
@@ -211,8 +280,10 @@ RSpec.describe "Jurassic Jigsaw", focus: true do
 ##.##..#.. ..#...#... .####...#.
 #.#.###... .##..##... .####.##.#
 #...###... ..##...#.. ...#..####
-..#.#....# ##.#.#.... ...##.....
-})
+..#.#....# ##.#.#.... ...##.....})
+      end
+      it do
+        expect(assemble(read_20(INPUTS20), 3643)).to eql(PUZZLE20)
       end
     end
   end
